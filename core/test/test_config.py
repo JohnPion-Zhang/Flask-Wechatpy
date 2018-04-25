53ce9a73404f17896d415aa41a0c2e98e5f0e2d6
JohnPion-Zhang <JohnPion.Zhang@gmail.com>
2018-04-25 16:20:34 +0800

    1. core initalize

---
+++ core/test/test_config.py
@@ -0,0 +1,15 @@
+import unittest
+import doctest
+from .. import config
+
+class TestConfig(unittest.TestCase):
+
+   def test_dev_config(self):
+      """ test development config
+         >>> 
+      """
+      self.assertEqual()
+
+if __name__ == '__main__':
+   doctest.testmod()
+   unittest.main()
